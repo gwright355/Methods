db27a4586d08847d2c68e0148755c7b3d4109a48
Glenn <Glenn@DESKTOP-76J73F3>
2019-01-31 17:14:36 -0500

    methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿// Glenn Wright
+// assignment: methods 
+// Ism 4300
+
+using System;
 
 namespace Methods
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                //User must enter name
+                Console.WriteLine("Please enter your name: ");
+
+                // Used to read user input
+                input(Console.ReadLine());
+
+            }// end of try
+            catch
+            {
+                //If user enters invalid data
+                Console.WriteLine("Please enter valid data");
+
+                Console.WriteLine("Press any key to exit the program and try again ...");
+                Console.ReadKey(true);
+            }// end of catch
+
+            
+
+            
+    }
+        //Method for displaying user name.
+          static void input(string name)
+        {
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey(true);
         }
+
     }
 }
